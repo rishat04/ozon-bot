a66223baa53778dc7de77959da96576975b473bd
rishat04 <rishat0404@gmail.com>
2022-05-17 11:21:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -614,7 +614,7 @@ def get_second_quantity(t):
 
 def scheduler():
     schedule.every().day.at('23:45').do(get_second_quantity, '23:45')
-    schedule.every().day.at('11:35').do(get_second_quantity, '00:00')
+    schedule.every().day.at('11:30').do(get_second_quantity, '00:00')
     schedule.every().day.at('06:00').do(get_second_quantity, '06:00')
     schedule.every().day.at('11:30').do(get_second_quantity, '12:00')
     schedule.every().day.at('18:03').do(get_second_quantity, '18:00')
